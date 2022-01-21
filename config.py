b226fe9e49ee4c6d4ae6be8b8d2282cbcca96cd7
slivkaia <Slivka-97@mail.ru>
2022-01-21 17:13:47 +0300

    commit 3

---
+++ config.py
@@ -0,0 +1,5 @@
+database="postgres"
+user="postgres"
+password="rootroot"
+host="127.0.0.1"
+port="5432"
--- parser.py
+++ parser.py
@@ -1,9 +1,29 @@
 from datetime import datetime, timedelta
 import re
-import calendar
-
 import requests
 from bs4 import BeautifulSoup as BS
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import host, user, password, database, port
+
+url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+engine = create_engine(url, echo=False)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Articles(Base):
+    __tablename__ = 'Articles'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    data = Column(String)
+
+
+Base.metadata.create_all(engine)
 
 name_mount = {
     "января": "01",
@@ -25,49 +45,61 @@ def convert_in_correct_form(convert):
     return "0" + str(convert) if len(str(convert)) == 1 else str(convert)
 
 
-def get_date(date: str):
-    if date.find("сегодня") >= 0:
+def get_date(data: str):
+    if data.find("сегодня") >= 0:
         d = datetime.now()
-        only_min_and_sec = date.replace('сегодня в ', "")
+        only_min_and_sec = data.replace('сегодня в ', "")
         year = d.year
         day = convert_in_correct_form(d.day)
         month = convert_in_correct_form(d.month)
-    elif date.find("вчера") >= 0:
-        only_min_and_sec = date.replace('вчера в ', "")
+    elif data.find("вчера") >= 0:
+        only_min_and_sec = data.replace('вчера в ', "")
         d = datetime.now() - timedelta(days=1)
         year = d.year
         day = convert_in_correct_form(d.day)
         month = convert_in_correct_form(d.month)
     else:
         d = datetime.now()
-        year = re.findall(r'\d{4}', date)
+        year = re.findall(r'\d{4}', data)
         if len(year) > 0:
             year = year[0]
         else:
             year = d.year
-        only_min_and_sec = date.split(" в ")[1]
-        day = date[0:2]
-        month = name_mount.get(re.findall(r'[а-яА-ЯёЁ]+', date)[0])
+        only_min_and_sec = data.split(" в ")[1]
+        day = data[0:2]
+        month = name_mount.get(re.findall(r'[а-яА-ЯёЁ]+', data)[0])
 
     correct_data = str(year) + "-" + month + "-" + day + " " + only_min_and_sec
-    date_formatter = "%Y-%m-%d %H:%M"
-    return datetime.strptime(correct_data, date_formatter)
+
+    return correct_data
 
 
-r = requests.get("https://bikepost.ru/index/page1/")
-html = BS(r.content, 'html.parser')
-t = html.find_all(class_="topic")
+next_page = True
+page = 1
+while next_page:
+    r = requests.get(f"https://bikepost.ru/index/page{page}/")
+    html = BS(r.content, 'html.parser')
 
-for el in t:
-    title = el.find(class_="title-topic")
-    author = el.find(class_="panel username")
-    no_correct_data = el.find(class_="panel username").find_next_sibling().find_next_sibling().text
-    if not no_correct_data[-1].isdigit():
-        no_correct_data = el.find(class_="panel username").find_next_sibling().text
+    max_page = html.find(class_='pagination').find_all('li')[9].text
+    max_page = max_page.replace("... ", "")
+    if max_page == page:
+        next_page = False
+    else:
+        page += 1
 
-    date = get_date(no_correct_data)
+    t = html.find_all(class_="topic")
+    for el in t:
+        title = el.find(class_="title-topic")
+        author = el.find(class_="panel username")
+        no_correct_data = el.find(class_="panel username").find_next_sibling().find_next_sibling().text
+        if not no_correct_data[-1].isdigit():
+            no_correct_data = el.find(class_="panel username").find_next_sibling().text
 
-    print(title.text)
-    print(author.text)
-    print(date)
-    print("------------------")
+        date = get_date(no_correct_data)
+
+        new_articles = session.query(Articles).filter(Articles.title == title.text, Articles.author == author.text,
+                                                      Articles.data == date).count()
+        if new_articles == 0:
+            art = Articles(title=title.text, author=author.text, data=date)
+            session.add(art)
+            session.commit()
